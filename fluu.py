66a6ecee0eee538d0f0b6eca20df3217a9043735
FluuDev <forsorayt@gmail.com>
2026-04-17 03:04:29 +0300

    Update fluu.py

--- fluu.py
+++ fluu.py
@@ -14,7 +14,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents, help_command=None)
 
 def load_config():
     try:
